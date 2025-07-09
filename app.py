7ec91e4ec2b8d89f50d91426d956cda7056ab66d
Grace Tao <grace@abc.com>
2025-07-09 14:07:09 -0400

    finished flask endpoint

--- app.py
+++ app.py
@@ -6,6 +6,25 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+expected_columns = ['homeworld_Alderaan', 'homeworld_Aleen Minor', 'homeworld_Bestine IV',
+       'homeworld_Cerea', 'homeworld_Champala', 'homeworld_Chandrila',
+       'homeworld_Concord Dawn', 'homeworld_Corellia', 'homeworld_Dagobah',
+       'homeworld_Dathomir', 'homeworld_Dorin', 'homeworld_Eriadu',
+       'homeworld_Glee Anselm', 'homeworld_Haruun Kal', 'homeworld_Iktotch',
+       'homeworld_Iridonia', 'homeworld_Kalee', 'homeworld_Kashyyyk',
+       'homeworld_Malastare', 'homeworld_Mirial', 'homeworld_Mon Cala',
+       'homeworld_Muunilinst', 'homeworld_Naboo', 'homeworld_Ojom',
+       'homeworld_Quermia', 'homeworld_Rodia', 'homeworld_Ryloth',
+       'homeworld_Serenno', 'homeworld_Shili', 'homeworld_Skako',
+       'homeworld_Socorro', 'homeworld_Stewjon', 'homeworld_Sullust',
+       'homeworld_Tatooine', 'homeworld_Tholoth', 'homeworld_Toydaria',
+       'homeworld_Trandosha', 'homeworld_Troiken', 'homeworld_Tund',
+       'homeworld_Umbara', 'homeworld_Vulpter', 'homeworld_Zolan',
+       'unit_type_at-at', 'unit_type_at-st', 'unit_type_resistance_soldier',
+       'unit_type_stormtrooper', 'unit_type_tie_fighter',
+       'unit_type_tie_silencer', 'unit_type_unknown', 'unit_type_x-wing']
+
+
 with open('best_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
@@ -14,9 +33,19 @@ def predict():
     data = request.get_json(force=True)
 
     if isinstance(data, dict):
+        print("here")
         data = [data]
+    data = pd.DataFrame(data)
 
-    prediction = model.predict(pd.DataFrame(data))
+    for column in expected_columns:
+        if column not in data.columns:
+            data[column] = 0
+
+    # Ensure correct order of columns
+    data = data[expected_columns]
+
+# Make prediction
+    prediction = model.predict(data)
 
     return jsonify(prediction.tolist())
 
