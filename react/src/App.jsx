627e6006c32aba59efef372e9911a857078b272a
Grace Tao <grace@abc.com>
2025-07-09 13:16:34 -0400

    added vite react app

---
+++ react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const handleSubmit = (e) => {
+
+  }
+
+  return (
+    <h1>Empire or Resistance?</h1>
+  )
+}
+
+export default App
