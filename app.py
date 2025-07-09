13a8357dfe4c7d2230c69067989681d2be8f6be2
Grace Tao <grace@abc.com>
2025-07-09 13:07:58 -0400

    tried grid search and created flask app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+with open('best_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/api/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+
+    if isinstance(data, dict):
+        data = [data]
+
+    prediction = model.predict(pd.DataFrame(data))
+
+    return jsonify(prediction.tolist())
+
+if __name__ == '__main__':
+    app.run(port=5000)
